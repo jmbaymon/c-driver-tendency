23c31d6c576529d95e3de5eff1ceb2f95801a32d
jmbaymon <jmbaymon@github.com>
2020-06-16 14:09:29 -0400

    add

---
+++ switch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    
+int a = 1;
+
+switch (a) {
+
+case 0:
+    printf("zero");
+    break;
+case 1:
+    printf("1");
+    break;
+default:
+    printf("Rice");
+break;
+
+}
+    return 0;
+}
+
---
+++ var.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int age = 0;
+
+    age = 37.2;
+    printf("%u",age);
+    return 0;
+}
